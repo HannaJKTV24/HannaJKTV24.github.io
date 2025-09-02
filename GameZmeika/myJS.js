74bacbf94258c8702872556674bdcf6951165c73
Hanna Zozulia <hanna.zozulia@ivkhk.ee>
2025-09-02 11:15:25 +0300

    Portfolio

--- GameZmeika/myJS.js
+++ GameZmeika/myJS.js
@@ -1,23 +1,25 @@
-var FIELD_SIZE_X = 20;
-var FIELD_SIZE_Y = 20;
-var SNAKE_SPEED = 300;
-var snake = [];
-var direction = 'y+';
-var gameIsRunning = false;
-var snake_timer;
-var food_timer;
-var score = 0;
+//Глобальные переменные:
+var FIELD_SIZE_X = 20; //строки
+var FIELD_SIZE_Y = 20; //столбцы
+var SNAKE_SPEED = 300; //Интервал между перемещениями змейки
+var snake = []; //сама змейка
+var direction = 'y+'; // направление движения змейки 
+var gameIsRunning = false; //запущена ли игра
+var snake_timer; //таймер змейки
+var food_timer; //таймер для еды
+var score = 0; // результат
 
 function init() {
-    prepareGameField();
+    prepareGameField(); //генерация поля
 
     var wrap = document.getElementsByClassName('wrap')[0];
 
     wrap.style.width = '400px';
-
+    //события кнопок Старт и Новая игра
     document.getElementById('snake-start').addEventListener('click', startGame);
     document.getElementById('snake-renew').addEventListener('click', refreshGame);
 
+    //отслеживания клавиш клавиатуры
     addEventListener('keydown', changeDirection);
     
 }
