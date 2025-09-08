6c40ee767bd19c2f07c262995345e5626dde761a
Hanna Zozulia <hanna.zozulia@ivkhk.ee>
2025-09-08 15:15:03 +0300

    Portfolio

--- Web_app/js/script.js
+++ Web_app/js/script.js
@@ -150,7 +150,85 @@ window.addEventListener('DOMContentLoaded', function() {
             this.changeToUSD();
         }
 
-        
+        changeToUSD() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+
+            if ( this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                <img scr=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена</div>
+                    <div class="menu__item-total"><span>${this.price}</span> EUR/день</div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
     }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.' + 
+        ' Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения,' +
+        'молоко из миндаля, овса, кокоса или гречки, правильное колтчество белков за счет тофу и импортных' +
+        'вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню "Премиум"',
+        'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.' +
+        'Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
+
+    //Forms
+
+    const forms = document.querySelectorAll('from');
+    const message = {
+        loading: 'img/form/spinner.svg',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            
+        })
+    }
+
+
 });
 
